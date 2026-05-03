4226c53e157b1fca6a7c2bc176df9986806e0415
highscroller <vga.22@uni-dubna.ru>
2026-05-04 01:13:29 +0300

    Доработан десктоп, выполнены задания по JS

---
+++ hw3.js
@@ -0,0 +1,61 @@
+// ЗАДАНИЕ 1
+
+let password = 'пароль';
+
+let input = prompt('Введите пароль');
+
+if (input === password) {
+    alert('Пароль введен верно');
+} else {
+    alert('Пароль введен неправильно');
+}
+
+// ЗАДАНИЕ 2
+
+let c = 67;
+
+if (c > 0 && c < 10) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+// ЗАДАНИЕ 3
+
+let d = 6;
+let e = 7;
+
+if (d > 100 || e > 100) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+
+// ЗАДАНИЕ 4
+
+let a = '2';
+let b = '3';
+// Код выше изменять менять нельзя. Чтобы решить задачу исправьте код ниже:
+alert(Number(a) + Number(b));
+
+// ЗАДАНИЕ 5
+
+let monthNumber = prompt('Введите номер месяца');
+
+switch (monthNumber) {
+    case '12' || '1' || '2':
+        alert('Зима');
+        break;
+    case '3' || '4' || '5':
+        alert('Весна');
+        break;
+    case '6' || '7' || '8':
+        alert('Лето');
+        break;
+    case '9' || '10' || '11':
+        alert('Осень');
+        break;
+    default:
+        alert('Неверный номер месяца');
+        break;
+}
