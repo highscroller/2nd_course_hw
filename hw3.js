6db5faca43f3e50e0a949612b874b380b18cc4b8
highscroller <vga.22@uni-dubna.ru>
2026-05-13 14:43:51 +0300

    Доработки по ДЗ-3

--- hw3.js
+++ hw3.js
@@ -43,19 +43,26 @@ alert(Number(a) + Number(b));
 let monthNumber = prompt('Введите номер месяца');
 
 switch (monthNumber) {
-    case '12' || '1' || '2':
+    case '12':
+    case '1':
+    case '2':
         alert('Зима');
         break;
-    case '3' || '4' || '5':
+    case '3':
+    case '4':
+    case '5':
         alert('Весна');
         break;
-    case '6' || '7' || '8':
+    case '6':
+    case '7':
+    case '8':
         alert('Лето');
         break;
-    case '9' || '10' || '11':
+    case '9':
+    case '10':
+    case '11':
         alert('Осень');
         break;
     default:
         alert('Неверный номер месяца');
-        break;
 }
